63aae0f7c28bdc6ee7ecfeb82435f6279dc46671
Edward Mortlock <e.mortlock@gmail.com>
2018-02-18 09:29:31 +0000

    Improved grid

--- client/components/Grid/components/GridWrapper.js
+++ client/components/Grid/components/GridWrapper.js
@@ -2,9 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
-const GridWrapper = ({ children, className }) => (
+const GridWrapper = ({ children, className, horizontallyCenter }) => (
   <div className={classnames('px-4', className)}>
-    <div className="flex flex-wrap -mx-4">
+    <div className={classnames('flex flex-wrap -mx-4', horizontallyCenter && 'justify-center')}>
       { children }
     </div>
   </div>
@@ -13,11 +13,13 @@ const GridWrapper = ({ children, className }) => (
 GridWrapper.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  horizontallyCenter: PropTypes.bool,
 }
 
 GridWrapper.defaultProps = {
   children: undefined,
   className: undefined,
+  horizontallyCenter: false,
 }
 
 export default GridWrapper
