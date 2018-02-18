de1f654858229d7d9c026463164875442e67c48c
Edward Mortlock <e.mortlock@gmail.com>
2018-02-18 11:40:49 +0000

    Got jest working

---
+++ client/components/Button/__tests__/components/ButtonLink.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { ButtonLink } from '../../'
+
+const setup = (testProps = {}) => {
+  const props = {
+    to: '/',
+    ...testProps,
+  }
+
+  const wrapper = shallow(<ButtonLink {...props}>Content</ButtonLink>)
+
+  return {
+    props,
+    wrapper,
+  }
+}
+
+describe('ButtonLink', () => {
+  it('renders matching snapshot', () => {
+    const { wrapper } = setup()
+
+    expect(wrapper).toMatchSnapshot()
+  })
+})
