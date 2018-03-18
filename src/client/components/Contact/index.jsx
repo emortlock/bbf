873f5a99f6f9f0a49d94fa771ef1be038906ccf4
Edward Mortlock <e.mortlock@gmail.com>
2018-03-18 18:11:06 +0000

    Smartened up contact mobile display

--- src/client/components/Contact/index.jsx
+++ src/client/components/Contact/index.jsx
@@ -4,6 +4,9 @@ import Post from '../../assets/images/contacts-post.svg'
 import Phone from '../../assets/images/contacts-phone.svg'
 import Email from '../../assets/images/contacts-email.svg'
 
+import PostIcon from '../../assets/icons/map-pin.svg'
+import PhoneIcon from '../../assets/icons/phone.svg'
+import EmailIcon from '../../assets/icons/email.svg'
 
 import { GridWrapper, GridItem } from '../Grid'
 import { H2 } from '../Heading'
@@ -12,10 +15,12 @@ import Section from '../Section'
 const Service = () => (
   <Section>
     <H2>Contact Us</H2>
-    <GridWrapper>
-      <GridItem className="w-full lg:w-1/3">
+    <GridWrapper horizontallyCenter>
+      <GridItem className="w-full md:w-1/2 lg:w-1/3">
         <a href="https://goo.gl/maps/oHpt1ur9B962" className="text-black hover:text-green">
-          <Post className="mb-2" height="8rem" width="8rem" />
+          <Post className="hidden md:inline-block mb-2" height="8rem" width="8rem" />
+          <PostIcon className="md:hidden fill-current h-10 w-10 mb-2" />
+
           <p>
             Berkeley Business Forms Ltd.<br />
             Freepost RLYH-RUJU-BBSJ<br />
@@ -27,16 +32,20 @@ const Service = () => (
         </a>
       </GridItem>
 
-      <GridItem className="w-full lg:w-1/3">
+      <GridItem className="w-full md:w-1/2 lg:w-1/3">
         <a href="tel:01244831462" className="text-black hover:text-green">
-          <Phone className="mb-2" height="8rem" width="8rem" />
+          <Phone className="hidden md:inline-block mb-2" height="8rem" width="8rem" />
+          <PhoneIcon className="md:hidden fill-current h-10 w-10 mb-2" />
+
           <p>01244 831462</p>
         </a>
       </GridItem>
 
-      <GridItem className="w-full lg:w-1/3">
+      <GridItem className="w-full md:w-1/2 lg:w-1/3">
         <a href="mailto:print@bbf.co.uk" className="text-black hover:text-green">
-          <Email className="mb-2" height="8rem" width="8rem" />
+          <Email className="hidden md:inline-block mb-2" height="8rem" width="8rem" />
+          <EmailIcon className="md:hidden fill-current h-10 w-10 mb-2" />
+
           <p>print@bbf.co.uk</p>
         </a>
       </GridItem>
