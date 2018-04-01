88af1abb7cd3727d0ad70ae824dbbc20634f4d63
Edward Mortlock <e.mortlock@gmail.com>
2018-04-01 09:29:07 +0100

    Used new config function

--- next.config.js
+++ next.config.js
@@ -14,66 +14,68 @@ class TailwindExtractor {
   }
 }
 
-module.exports = withBundleAnalyzer(withCSS(withMdxc({
-  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  analyzeClient: ['client', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  webpack: (config, { dev, isServer }) => {
-    const newConfig = Object.assign({}, config)
-    const originalEntry = config.entry
+module.exports = (phase, { defaultConfig }) =>
+  withBundleAnalyzer(withCSS(withMdxc({
+    ...defaultConfig,
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeClient: ['client', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    webpack: (config, { dev, isServer }) => {
+      const newConfig = Object.assign({}, config)
+      const originalEntry = config.entry
 
-    newConfig.entry = async () => {
-      const entries = await originalEntry()
+      newConfig.entry = async () => {
+        const entries = await originalEntry()
 
-      if (entries['main.js']) {
-        entries['main.js'].unshift('babel-polyfill')
+        if (entries['main.js']) {
+          entries['main.js'].unshift('babel-polyfill')
+        }
+
+        return entries
       }
 
-      return entries
-    }
+      newConfig.resolve.extensions = ['.js', '.jsx']
 
-    newConfig.resolve.extensions = ['.js', '.jsx']
+      if (!dev && !isServer) {
+        newConfig.plugins.push(
+          new PurgecssPlugin({
+            whitelist: ['html', 'body', 'c-heading--divider-white'],
+            whitelistPatterns: [/-[0-9]\/[0-9]$/],
+            paths: glob.sync([
+              path.join(__dirname, '/src/client/**/*.js'),
+              path.join(__dirname, '/src/client/**/*.jsx'),
+            ]),
+            extractors: [{
+              extractor: TailwindExtractor,
+              extensions: ['html', 'js', 'jsx'],
+            }],
+          }),
+        )
+      }
 
-    if (!dev && !isServer) {
-      newConfig.plugins.push(
-        new PurgecssPlugin({
-          whitelist: ['html', 'body', 'c-heading--divider-white'],
-          whitelistPatterns: [/-[0-9]\/[0-9]$/],
-          paths: glob.sync([
-            path.join(__dirname, '/src/client/**/*.js'),
-            path.join(__dirname, '/src/client/**/*.jsx'),
-          ]),
-          extractors: [{
-            extractor: TailwindExtractor,
-            extensions: ['html', 'js', 'jsx'],
-          }],
-        }),
-      )
-    }
-
-    return newConfig
-  },
-  exportPathMap() {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/contact': { page: '/contact' },
-      '/guarantee': { page: '/guarantee' },
-      '/products': { page: '/products' },
-      '/products/business-stationery': { page: '/products/business-stationery' },
-      '/products/continuous-stationery': { page: '/products/continuous-stationery' },
-      '/products/digital-printing': { page: '/products/digital-printing' },
-      '/products/envelopes': { page: '/products/envelopes' },
-      '/products/laser-sheets': { page: '/products/laser-sheets' },
-      '/products/miscellaneous': { page: '/products/miscellaneous' },
-      '/products/one-piece-mailers': { page: '/products/one-piece-mailers' },
-      '/products/pads-sets-books': { page: '/products/pads-sets-books' },
-      '/products/personalisation': { page: '/products/personalisation' },
-      '/products/plastic-products': { page: '/products/plastic-products' },
-      '/products/promotional-gifts': { page: '/products/promotional-gifts' },
-      '/products/promotional-print': { page: '/products/promotional-print' },
-      '/products/security-products': { page: '/products/security-products' },
-      '/products/self-adhesive-products': { page: '/products/self-adhesive-products' },
-      '/products/special-products': { page: '/products/special-products' },
-    }
-  },
-})))
+      return newConfig
+    },
+    exportPathMap() {
+      return {
+        '/': { page: '/' },
+        '/about': { page: '/about' },
+        '/contact': { page: '/contact' },
+        '/guarantee': { page: '/guarantee' },
+        '/products': { page: '/products' },
+        '/products/business-stationery': { page: '/products/business-stationery' },
+        '/products/continuous-stationery': { page: '/products/continuous-stationery' },
+        '/products/digital-printing': { page: '/products/digital-printing' },
+        '/products/envelopes': { page: '/products/envelopes' },
+        '/products/laser-sheets': { page: '/products/laser-sheets' },
+        '/products/miscellaneous': { page: '/products/miscellaneous' },
+        '/products/one-piece-mailers': { page: '/products/one-piece-mailers' },
+        '/products/pads-sets-books': { page: '/products/pads-sets-books' },
+        '/products/personalisation': { page: '/products/personalisation' },
+        '/products/plastic-products': { page: '/products/plastic-products' },
+        '/products/promotional-gifts': { page: '/products/promotional-gifts' },
+        '/products/promotional-print': { page: '/products/promotional-print' },
+        '/products/security-products': { page: '/products/security-products' },
+        '/products/self-adhesive-products': { page: '/products/self-adhesive-products' },
+        '/products/special-products': { page: '/products/special-products' },
+      }
+    },
+  })))
